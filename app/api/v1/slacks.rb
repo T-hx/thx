f417834879bc5d188e843c2483f57cdd9a7cf834
shun takagi <s.takagi.910@gmail.com>
2018-07-28 14:33:30 +0900

    ユーザーをコマンドで登録

--- app/api/v1/slacks.rb
+++ app/api/v1/slacks.rb
@@ -17,6 +17,20 @@ module Slacks
           }
         end
 
+        # POST /v1/slacks/thxes
+        desc 'ポイントの確認'
+        params do
+          requires :team_id, type: String, desc: 'チームID'
+          requires :user_id, type: String, desc: 'ユーザID'
+        end
+        post '/' do
+          st_params = strong_params(params).permit(:team_id, :user_id)
+          user = User.find_by(slack_team_id: st_params[:team_id], slack_user_id: st_params[:user_id])
+          {
+            text: "ポイント残高: #{user.thx_balance} \n みんなからもらったポイント: #{user.received_thx}"
+          }
+        end
+
         # POST /v1/slack/thxes/send
         desc 'ポイントの送信'
         params do
@@ -62,6 +76,42 @@ module Slacks
                   text: '/thx @送る相手 ポイント メッセージ'
                 }
               ]
+            }
+          end
+        end
+      end
+
+      resource 'user' do
+        # POST /v1/slacks/thxes
+        desc 'ユーザーの追加'
+        params do
+          requires :team_id, type: String, desc: 'チームID'
+          requires :user_id, type: String, desc: 'ユーザID'
+        end
+        post 'add' do
+          st_params = strong_params(params).permit(:team_id, :user_id)
+          user = User.find_by(slack_team_id: st_params[:team_id], slack_user_id: st_params[:user_id])
+          if user.present?
+            {
+              text: "#{user.name}はすでに登録されています"
+            }
+          else
+            res = Net::HTTP.get(URI.parse("https://slack.com/api/users.info?token=#{ENV['SLACK_TOKEN']}&user=#{st_params[:user_id]}&pretty=1"))
+            pretty_res = JSON.parse(res)
+            ApplicationRecord::transaction do
+              user = User.new(name: pretty_res['name'],
+                              email: pretty_res['profile']['email'],
+                              slack_user_id: pretty_res['id'],
+                              slack_team_id: pretty_res['team_id'],
+                              address: SecureRandom.hex,
+                              thx_balance: User::INIT_THX,
+                              password: User::SLACK_USER_DUMMY_PASSWORD,
+                              password_confirmation: User::SLACK_USER_DUMMY_PASSWORD,
+                              verified: true)
+              user.save!
+            end
+            {
+              text: "pretty_res['name']さん、thxへようこそ! :tada:"
             }
           end
         end
