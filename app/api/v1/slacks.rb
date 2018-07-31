48839f7706b7bfe66f9f19e393b69e8d3f33ba24
shun takagi <s.takagi.910@gmail.com>
2018-08-01 00:08:27 +0900

    test Interactive Messages

--- app/api/v1/slacks.rb
+++ app/api/v1/slacks.rb
@@ -110,6 +110,56 @@ module Slacks
         end
         post 'help' do
           {
+            "text": "Would you like to play a game?",
+            "attachments": [
+              {
+                "text": "Choose a game to play",
+                "fallback": "You are unable to choose a game",
+                "callback_id": "wopr_game",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                  {
+                    "name": "game",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                  },
+                  {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                  },
+                  {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                      "title": "Are you sure?",
+                      "text": "Wouldn't you prefer a good game of chess?",
+                      "ok_text": "Yes",
+                      "dismiss_text": "No"
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        end
+
+        # POST /v1/slacks/thxes/stamp
+        desc 'ボタンが押された時'
+        params do
+          requires :team_id, type: String, desc: 'チームID'
+          requires :user_id, type: String, desc: 'ユーザID'
+        end
+        post 'stamp' do
+          st_params = strong_params(params)
+          puts st_params
+          {
             text: "This command is not available and the help page is still on the development.:man-bowing::skin-tone-4:"
           }
         end
