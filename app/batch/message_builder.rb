1a7c59ac024fca4184b0f5997144083be687ebf2
Shun Takagi <co_shun-takagi@c-fo.com>
2018-08-06 14:34:09 +0900

    Fixed weekly ranking script.

--- app/batch/message_builder.rb
+++ app/batch/message_builder.rb
@@ -12,7 +12,7 @@ module MessageBuilder
 
   def build_sent_count_ranking(st, en)
     best_3 = ThxTransaction.where(created_at: st..en).where.not(sender: nil).
-      group(:sender).count.sort_by {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
+      group(:sender).count.sort {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
     best_3.map.with_index(1) do |data, index|
       "*Best#{index} #{MEDAL_STAMP[index - 1]}* \n *#{data.first.name}* \n送信回数 #{data.second}回 \n"
     end.unshift("*送信回数ランキング* :outbox_tray: =======\n").join("\n")
@@ -20,7 +20,7 @@ module MessageBuilder
 
   def build_thx_amount_ranking(st, en)
     best_3 = ThxTransaction.where(created_at: st..en).where.not(receiver: nil).
-      group(:receiver).sum(:thx).sort_by {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
+      group(:receiver).sum(:thx).sort {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
     best_3.map.with_index(1) do |data, index|
       "*Best#{index} #{MEDAL_STAMP[index - 1]}* \n *#{data.first.name}* \nthxポイント量 #{data.second}thx \n"
     end.unshift("*受信thxポイント量ランキング* =======\n").join("\n")
