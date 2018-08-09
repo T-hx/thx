bbe11cc36ae8bbce1eefa7741ebf890d63c6deea
Shun Takagi <co_shun-takagi@c-fo.com>
2018-08-09 12:19:25 +0900

    [WIP] refactoring slack report script

---
+++ app/batch/slack_reporter/base.rb
@@ -0,0 +1,18 @@
+module SlackReport
+  class Base
+    include Batch
+    extend MessageBuilder
+
+    def self.report
+      @logger.info '[start] start thx weekly ranking'
+      notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
+      notifier.ping(build_message)
+      @logger.info '[end] end thx weekly ranking'
+    end
+
+    def self.build_message
+      raise 'Called abstract method'
+    end
+  end
+end
+
