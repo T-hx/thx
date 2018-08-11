9aaef094b4b7ee1b4d68b001da9f9db55e1c6e03
shun takagi <s.takagi.910@gmail.com>
2018-08-12 01:17:59 +0900

    slack_report#テンプレート修正

--- app/batch/slack_reporter/message_builder.rb
+++ app/batch/slack_reporter/message_builder.rb
@@ -8,7 +8,7 @@ module SlackReport
         group(:receiver).count.sort {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
       best_3.map.with_index(1) do |data, index|
         "*Best#{index} #{MEDAL_STAMP[index - 1]}* \n *#{data.first.name}* \n受信回数 #{data.second}回 \n"
-      end.unshift("*受信回数ランキング* :inbox_tray: =======\n").join("\n")
+      end.join("\n")
     end
 
     def build_sent_count_ranking(st, en)
@@ -16,7 +16,7 @@ module SlackReport
         group(:sender).count.sort {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
       best_3.map.with_index(1) do |data, index|
         "*Best#{index} #{MEDAL_STAMP[index - 1]}* \n *#{data.first.name}* \n送信回数 #{data.second}回 \n"
-      end.unshift("*送信回数ランキング* :outbox_tray: =======\n").join("\n")
+      end.join("\n")
     end
 
     def build_thx_amount_ranking(st, en)
@@ -24,7 +24,7 @@ module SlackReport
         group(:receiver).sum(:thx).sort {|(k1, v1), (k2, v2)| v2 <=> v1}.take(3)
       best_3.map.with_index(1) do |data, index|
         "*Best#{index} #{MEDAL_STAMP[index - 1]}* \n *#{data.first.name}* \nthxポイント量 #{data.second}thx \n"
-      end.unshift("*受信thxポイント量ランキング* =======\n").join("\n")
+      end.join("\n")
     end
   end
 end
--- app/batch/slack_reporter/ranking/ranking.rb
+++ app/batch/slack_reporter/ranking/ranking.rb
@@ -6,10 +6,29 @@ module SlackReport
     END_DATE = Date.yesterday.end_of_week
     
     def self.build_message
-      "*Thx Weekly Ranking (#{START_DATE}~#{END_DATE})* :tada:\n" +
-        build_received_count_ranking(START_DATE, END_DATE) + "\n" +
-        build_sent_count_ranking(START_DATE, END_DATE) + "\n" +
-        build_thx_amount_ranking(START_DATE, END_DATE) + "\n"
+      {
+        "text": "*Thx Weekly Ranking (#{START_DATE}~#{END_DATE})* :tada:",
+        "attachments": [
+          {
+            "color": "good",
+            "title": "*受信回数ランキング* :inbox_tray: =======\n",
+            "title_link": "https://api.slack.com/",
+            "text": build_received_count_ranking(START_DATE, END_DATE)
+          },
+          {
+            "color": "good",
+            "title": "*送信回数ランキング* :inbox_tray: =======\n",
+            "title_link": "https://api.slack.com/",
+            "text": build_sent_count_ranking(START_DATE, END_DATE)
+          },
+          {
+            "color": "good",
+            "title": "*thx獲得量ランキング* :inbox_tray: =======\n",
+            "title_link": "https://api.slack.com/",
+            "text": build_thx_amount_ranking(START_DATE, END_DATE)
+          }
+        ]
+      }
     end
   end
 
