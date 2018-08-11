cc872edea78a4c52217a5aeb711542c58aeb2757
shun takagi <s.takagi.910@gmail.com>
2018-08-12 01:25:17 +0900

    slack_report#ランキング#テンプレート修正

--- app/batch/slack_reporter/ranking/ranking.rb
+++ app/batch/slack_reporter/ranking/ranking.rb
@@ -1,6 +1,6 @@
 module SlackReport
   class WeeklyThxRanking < Base
-    extend MessageBuilder
+    extend Ranking::MessageBuilder
 
     START_DATE = Date.yesterday.beginning_of_week
     END_DATE = Date.yesterday.end_of_week
@@ -11,19 +11,19 @@ module SlackReport
         "attachments": [
           {
             "color": "good",
-            "title": "*受信回数ランキング* :inbox_tray: =======\n",
+            "title": "受信回数ランキング :inbox_tray: =======\n",
             "title_link": "https://api.slack.com/",
             "text": build_received_count_ranking(START_DATE, END_DATE)
           },
           {
             "color": "good",
-            "title": "*送信回数ランキング* :inbox_tray: =======\n",
+            "title": "送信回数ランキング :inbox_tray: =======\n",
             "title_link": "https://api.slack.com/",
             "text": build_sent_count_ranking(START_DATE, END_DATE)
           },
           {
             "color": "good",
-            "title": "*thx獲得量ランキング* :inbox_tray: =======\n",
+            "title": "thx獲得量ランキング :inbox_tray: =======\n",
             "title_link": "https://api.slack.com/",
             "text": build_thx_amount_ranking(START_DATE, END_DATE)
           }
