eb61e4b22b2c6501b1d3de8f5ac0accb5fb45a91
shun takagi <s.takagi.910@gmail.com>
2018-08-12 01:37:05 +0900

    slack_report#Base#ログ文言修正

--- app/batch/slack_reporter/base.rb
+++ app/batch/slack_reporter/base.rb
@@ -3,13 +3,13 @@ module SlackReport
     include ::Batch
 
     def self.report
-      puts '[start] start thx weekly ranking'
+      puts '[start] start report'
       notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
       notifier.ping(build_message)
     rescue => e
       puts "[error] #{e.inspect}\n#{e.backtrace.join("\n")}"
     ensure
-      puts "[end] end thx weekly ranking"
+      puts '[end] end report'
     end
 
     def self.build_message
