ef89817073e2dae23556f583e3d314d81ce35e24
shun takagi <s.takagi.910@gmail.com>
2018-08-14 01:33:09 +0900

    slack_repoter#ranking#spec追加&リファクタ

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,12 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email){|i| "example#{i}@example.com"}
-    password "pass-word0000"
-    password_confirmation "pass-word0000"
+    sequence(:email){|i| "example#{i}@example.com" }
+    password 'pass-word000'
+    sequence(:name){|i| "user#{i}"}
     address { SecureRandom.hex }
     thx_balance 1000
-    status "enable"
-  end
-
-  trait :sign_in do
-    after(:create) do |user|
-      create(:access_token, user: user)
-    end
+    sequence(:received_thx){|i| i*100}
+    sequence(:slack_user_id){|i| "#{i}"}
+    sequence(:slack_team_id){|i| 'a' }
   end
 end
