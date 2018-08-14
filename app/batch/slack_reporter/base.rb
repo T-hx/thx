055697a1e6a04c7d1360a48b248787488e0430c8
shun takagi <s.takagi.910@gmail.com>
2018-08-15 00:00:54 +0900

    slack_repoter#ログにクラス名を含める

--- app/batch/slack_reporter/base.rb
+++ app/batch/slack_reporter/base.rb
@@ -3,14 +3,14 @@ module SlackReporter
     include ::Batch
     class << self
       def report
-        puts '[start] start report'
+        puts "[start] #{self.name} report start"
         notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
         notifier.ping(build_message)
-        puts '[success] success report'
+        puts "[success] #{self.name} report success"
       rescue => e
         puts "[error] #{e.inspect}\n#{e.backtrace.join("\n")}"
       ensure
-        puts '[end] end report'
+        puts "[end] #{self.name} report end"
       end
 
       def build_message
