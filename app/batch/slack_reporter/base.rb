45b1219fa046249a07f43b8c6e49af1a808f661f
shun takagi <s.takagi.910@gmail.com>
2018-11-29 22:53:42 +0900

    fixed slack_reporter

--- app/batch/slack_reporter/base.rb
+++ app/batch/slack_reporter/base.rb
@@ -3,14 +3,8 @@ module SlackReporter
     include ::Batch
     class << self
       def run
-        puts "[start] #{self.name} report start"
         notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
         notifier.ping(build_message)
-        puts "[success] #{self.name} report success"
-      rescue => e
-        puts "[error] #{e.inspect}\n#{e.backtrace.join("\n")}"
-      ensure
-        puts "[end] #{self.name} report end"
       end
 
       def build_message
